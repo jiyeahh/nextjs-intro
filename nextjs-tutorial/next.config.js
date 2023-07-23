c2d477c18e01c07ad2121ddddebcb04dbf0fac31
jiyeahh <mwldpm@naver.com>
2023-07-23 16:35:38 +0900

    add: SSR and redirect,rewrite in nextconfig

--- nextjs-tutorial/next.config.js
+++ nextjs-tutorial/next.config.js
@@ -1,6 +1,26 @@
+const API_KEY = process.env.API_KEY;
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects(){
+    return[
+      {
+        source:"/old-blg/:path*",
+        destination:"/new-blg/:path*",
+        permanent: false,
 
+      },
+    ]
+  },
+  async rewrites(){
+    return[{
+      source:"/api/movies",
+      destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+    }]
+  }
+};
+//redirect는 source 페이지로 가면 destination으로 이동 뒤의 path는 catch를 위함
+//rewirtes는 기존 페이지에서 
 module.exports = nextConfig
